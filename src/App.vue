a6883e65472f16680f40b3dff32ef0df53027513
garry-555 <kumargauravbvp@gmail.com>
2021-09-27 12:55:17 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <section>
+    <h2>my friends</h2>
+    <ul>
+      <friend-contact> </friend-contact>
+      <friend-contact> </friend-contact>
+    </ul>
+  </section>
+</template>
+
+<script>
+import FriendContact from "./components/FriendContact.vue";
+export default {
+  components: { FriendContact },
+  data() {
+    return {
+      friends: [
+        {
+          id: "garry",
+          name: "gaurav",
+          phone: "89998",
+          emailId: "krgmail.com",
+        },
+        {
+          id: "saf",
+          name: "safder",
+          phone: "77675",
+          emailId: "safgmail.com",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+
---
+++ src/components/FriendContact.vue
@@ -0,0 +1,33 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">show details</button>
+    <ul v-if="isVisible">
+      <li><strong>phone: </strong> {{ friend.phone }}</li>
+      <!-- <li><strong><strong> {{}}</li> -->
+      <li><strong>email: </strong> {{ friend.emailId }}</li>
+    </ul>
+  </li>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      isVisible: false,
+      friend: {
+        id: "garry",
+        name: "gaurav",
+        phone: "89998",
+        emailId: "krgmail.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.isVisible = !this.isVisible;
+    },
+  },
+};
+</script>
